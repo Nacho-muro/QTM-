6d8967e702c5ca93f5e9130586e382e38ad63fa3
Nacho-muro <nachomurilloperes@gmail.com>
2025-06-13 18:09:03 +0200

    Update simulador_valoracion_finnhub (1).py

--- simulador_valoracion_finnhub (1).py
+++ simulador_valoracion_finnhub (1).py
@@ -1,3 +1,4 @@
 
 # TU CÓDIGO COMPLETO VA AQUÍ (recortado aquí por espacio)
 # Puedes pedirme dividirlo en bloques si lo prefieres en partes más pequeñas
+streamlit run simulador_valoracion_finnhub.py
